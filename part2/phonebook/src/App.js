20295cf24b96135e77aca854a5f32e417fbfc2d4
Damilola Michael Ige <idm.calculus@gmail.com>
2020-05-29 18:51:31 +0100

    fetches initial app state from the json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [ persons, setPersons ] = useState([]);
   const [ newName, setNewName ] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(()=>{
+	axios
+	.get('http://localhost:3004/persons')
+	.then(response => {
+		setPersons(response.data);
+	})
+	.catch(error => console.log(error));
+}, []);
 
   const addName = (event) => {
 	  event.preventDefault();
